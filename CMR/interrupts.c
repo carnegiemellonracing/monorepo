d0adcb0d8235d957991944198f26e23c3d3b5c80
Stan Zhang <stan.zhang2@gmail.com>
2019-01-25 22:32:13 -0500

    Interrupt handler overrides

---
+++ CMR/interrupts.c
@@ -0,0 +1,43 @@
+/**
+ * @file interrupts.c
+ * @brief Mandatory common interrupt handler implementations.
+ *
+ * These definitions override the default handlers for each interrupt, which
+ * spins forever.
+ *
+ * The default handlers are (weakly) defined in `CMSIS/startup_stm32f413xx.s`.
+ *
+ * @author Carnegie Mellon Racing
+ */
+
+#include <stm32f4xx_hal.h>  // HAL_IncTick()
+
+/**
+ * @brief Port-specific system tick handler; provided by FreeRTOS.
+ *
+ * @see FreeRTOS/portable/GCC/ARM_CM4F/port.c
+ */
+extern void xPortSysTickHandler(void);
+
+/**
+ * @brief System tick interrupt handler.
+ */
+void SysTick_Handler(void) {
+    HAL_IncTick();  // Report tick to HAL.
+    xPortSysTickHandler();  // Invoke FreeRTOS tick handler.
+}
+
+/**
+ * @brief Non-maskable interrupt handler.
+ */
+void NMI_Handler(void) {
+    // Nothing to do.
+}
+
+/**
+ * @brief Debug monitor exception handler.
+ */
+void DebugMon_Handler(void) {
+    // Nothing to do.
+}
+
