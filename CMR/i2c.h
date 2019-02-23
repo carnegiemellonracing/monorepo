c5848d4e6c2c832a0ece6b2fbfcc36c776443547
jxu12345 <jiachenx@andrew.cmu.edu>
2019-02-22 20:11:24 -0500

    constructed .h file with init function

--- CMR/i2c.h
+++ CMR/i2c.h
@@ -4,10 +4,24 @@
  *  Created on: Feb 22, 2019
  *      Author: jason
  */
-
 #ifndef CMR_I2C_H_
 #define CMR_I2C_H_
 
+#include <stm32f4xx_hal.h>
+#include <stddef.h>
+#include <stdint.h>
 
+
+
+typedef struct {
+	I2C_HandleTypeDef handle;     /**< @brief HAL I2C Handle.*/
+	uint16_t *addresses;           /**< @brief Configured I2C devices.*/
+	size_t addressLen;            /**< @brief Number of I2C devices.*/
+}cmr_i2c_t;
+
+void cmr_i2cInit(
+    cmr_i2c_t *i2c, I2C_TypeDef *instance,
+    uint16_t *addresses, const size_t addressLen
+);
 
 #endif /* CMR_I2C_H_ */
