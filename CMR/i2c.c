5c8f916b8943bc8928477260b448f74fb66f925e
jxu12345 <jiachenx@andrew.cmu.edu>
2019-02-24 15:10:32 -0500

    Update i2c.c
    Fixed grammar issues

--- CMR/i2c.c
+++ CMR/i2c.c
@@ -40,7 +40,7 @@ int cmr_i2cTX(uint16_t devAddr, uint8_t *data, int dataLength){
 void cmr_i2cInit(
     cmr_i2c_t *i2c, I2C_Typedef *instance,
     uint32_t clockSpeed, uint32_t ownAddr,
-    uint16_t *devAddr, const size_t addrLen
+    uint16_t *devAddr, const size_t addrLen,
     GPIO_Typedef *i2cPort, uint32_t i2cPin
 ) {
 	//TODO: error messages for what could go wrong here
@@ -49,23 +49,23 @@ void cmr_i2cInit(
             .Instance = instance,
             // Configure I2C in low frequency mode, copied from stm32bringups
             .Init = {
-            		.ClockSpeed = clockSpeed,
-            		.DutyCycle = I2C_DUTYCYCLE_2,
-            		.OwnAddress1 = ownAddr,
-            		.AddressingMode = I2C_ADDRESSINGMODE_7BIT,
-            		.DualAddressMode = I2C_DUALADDRESS_DISABLE,
-            		.OwnAddress2 = 0,
-            		.GeneralCallMode = I2C_GENERALCALL_DISABLE,
-            		.NoStretchMode = I2C_NOSTRETCH_DISABLE
+            	.ClockSpeed = clockSpeed,
+            	.DutyCycle = I2C_DUTYCYCLE_2,
+            	.OwnAddress1 = ownAddr,
+            	.AddressingMode = I2C_ADDRESSINGMODE_7BIT,
+            	.DualAddressMode = I2C_DUALADDRESS_DISABLE,
+            	.OwnAddress2 = 0,
+            	.GeneralCallMode = I2C_GENERALCALL_DISABLE,
+            	.NoStretchMode = I2C_NOSTRETCH_DISABLE
             }
         },
         .devAddr = devAddr,
         .addrLen = addrLen
     };
 
-     if (HAL_I2C_Init(&i2c->handle) != HAL_OK) {
+  if (HAL_I2C_Init(&i2c->handle) != HAL_OK) {
         cmr_panic("HAL_I2C_Init() failed!");
-    }
+  }
   
   cmr_rccGPIOClockEnable(i2cPort);
 
@@ -74,13 +74,13 @@ void cmr_i2cInit(
   HAL_GPIO_WritePin(GPIOB, i2cPin, GPIO_PIN_RESET);
 
 
-// Format copied from can.c, data copied from stm32-bringups
+  // Format copied from can.c, data copied from stm32-bringups
   GPIO_InitTypeDef pinConfig = {
     .Pin = i2cPin,
     .Mode = GPIO_MODE_OUTPUT_PP,
     .Pull = GPIO_NOPULL,
     .Speed = GPIO_SPEED_FREQ_LOW
-  }
+  };
 
    HAL_GPIO_Init(i2cPin, &pinConfig);
 
