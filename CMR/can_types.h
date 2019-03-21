ae5ba35afca486202f1e5cfc6fba525e44507343
Stan Zhang <stan.zhang2@gmail.com>
2019-03-21 16:21:35 -0400

    Make CAN unknown state 0 for default initialization (#17)

--- CMR/can_types.h
+++ CMR/can_types.h
@@ -12,12 +12,12 @@
 
 /** @brief Node states. */
 typedef enum {
+    CMR_CAN_UNKNOWN = 0,    /**< @brief Current state unknown. */
     CMR_CAN_GLV_ON,         /**< @brief Grounded low voltage on. */
     CMR_CAN_HV_EN,          /**< @brief High voltage enabled. */
     CMR_CAN_RTD,            /**< @brief Ready to drive. */
     CMR_CAN_ERROR,          /**< @brief Error has occurred. */
-    CMR_CAN_CLEAR_ERROR,    /**< @brief Request to clear error. */
-    CMR_CAN_UNKNOWN         /**< @brief Current state unknown. */
+    CMR_CAN_CLEAR_ERROR     /**< @brief Request to clear error. */
 } cmr_canState_t;
 
 /** @brief Fan states. */
