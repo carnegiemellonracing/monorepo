62f444e2e99d3286b4fcec6b75c051f1675004fb
Cameron Mackintosh <cmackint@posteo.net>
2019-03-22 11:31:22 -0400

    Debugging

--- CMR/config.c
+++ CMR/config.c
@@ -10,10 +10,27 @@
 #include <stm32f4xx_hal.h>
 
 #define CONFIG_END_ADDR 1024
-#define FLASH_START_ADDR 0x08160000
+#define FLASH_START_ADDR 0x08040000
 #define FLASH_END_ADDR (FLASH_START_ADDR + CONFIG_END_ADDR)
 
-static const volatile uint8_t *flash = (const volatile uint8_t *) FLASH_START_ADDR;
+#define ADDR_FLASH_SECTOR_0     ((uint32_t)0x08000000) 
+#define ADDR_FLASH_SECTOR_1     ((uint32_t)0x08004000) 
+#define ADDR_FLASH_SECTOR_2     ((uint32_t)0x08008000) 
+#define ADDR_FLASH_SECTOR_3     ((uint32_t)0x0800C000) 
+#define ADDR_FLASH_SECTOR_4     ((uint32_t)0x08010000) 
+#define ADDR_FLASH_SECTOR_5     ((uint32_t)0x08020000) 
+#define ADDR_FLASH_SECTOR_6     ((uint32_t)0x08040000) 
+#define ADDR_FLASH_SECTOR_7     ((uint32_t)0x08060000) 
+#define ADDR_FLASH_SECTOR_8     ((uint32_t)0x08080000) 
+#define ADDR_FLASH_SECTOR_9     ((uint32_t)0x080A0000) 
+#define ADDR_FLASH_SECTOR_10    ((uint32_t)0x080C0000) 
+#define ADDR_FLASH_SECTOR_11    ((uint32_t)0x080E0000) 
+#define ADDR_FLASH_SECTOR_12    ((uint32_t)0x08100000) 
+#define ADDR_FLASH_SECTOR_13    ((uint32_t)0x08120000) 
+#define ADDR_FLASH_SECTOR_14    ((uint32_t)0x08140000) 
+#define ADDR_FLASH_SECTOR_15    ((uint32_t)0x08160000) 
+
+static const volatile uint32_t *flash = (const volatile uint32_t *) FLASH_START_ADDR;
 static volatile uint32_t configBuf[CONFIG_END_ADDR];
 
 void HAL_FLASH_EndOfOperationCallback(uint32_t returnValue) {
@@ -68,8 +85,18 @@ void cmr_configCommit() {
 
     __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | \
             FLASH_FLAG_PGAERR | FLASH_FLAG_PGSERR);
-    
-    FLASH_Erase_Sector(FLASH_SECTOR_6, VOLTAGE_RANGE_3);
+   
+    FLASH_EraseInitTypeDef eraseInit = {
+        .TypeErase = FLASH_TYPEERASE_SECTORS,
+        .Sector = FLASH_SECTOR_6,
+        .NbSectors = 1,
+        .VoltageRange = VOLTAGE_RANGE_3,
+    };
+
+    uint32_t error;
+    if (HAL_FLASHEx_Erase(&eraseInit, &error) != HAL_OK) {
+        cmr_panic("Flash erase failed!");
+    }
 
     size_t idx = 0;
     for (size_t addr = FLASH_START_ADDR; addr < FLASH_END_ADDR; addr += sizeof(uint32_t)) {
