3a4a8989c15ac2cdde437b7be14379ce4a1727da
Stan Zhang <stan.zhang2@gmail.com>
2019-04-03 22:34:24 -0400

    Streamlined sensors interface, add error flag

--- CMR/sensors.c
+++ CMR/sensors.c
@@ -6,55 +6,39 @@
  */
 
 #include "sensors.h"    // Interface to implement
-#include <stddef.h> // NULL
-
-
-/**
- * @brief Initializes the sensor if it hasn't been initialized yet.
- *
- * @param sensor The sensor to initialize.
- */
-void cmr_initializeSensor(cmr_sensor_t* sensor) {
-    if (sensor->isInitialized == true) {
-        return;
-    }
-    if (sensor->sensorInit != NULL) {
-        sensor->sensorInit(sensor);
-    }
-    sensor->isInitialized = true;
-}
+#include <stddef.h>     // NULL
 
 /**
  * @brief Calculates how much a reading can be either below the min reading value or above the
  * max reading value.
  *
+ * @param sensor The sensor.
+ *
  * @return The amount that a reading can be outside of the normal reading range.
  */
-static int32_t cmr_getReadingTolerance(cmr_sensor_t* sensor) {
+static int32_t cmr_getReadingTolerance(cmr_sensor_t *sensor) {
     int32_t readingRange = sensor->maxReading - sensor->minReading;
     return readingRange / 100 * sensor->warnThres_pcnt;
 }
 
 /**
- * @brief Stores a converted sensor value and clears the error code if
- * the value is valid - otherwise the error code is set, but the sensor
- * value is still stored.
+ * @brief Stores a converted sensor value and clears the error code if the value
+ * is valid - otherwise the error code is set, but the sensor value is still
+ * stored.
  *
  * @param sensor The sensor to get a value for.
- *
  */
-void cmr_getSensorValue(cmr_sensor_t* sensor) {
-    cmr_initializeSensor(sensor);
-
+void cmr_getSensorValue(cmr_sensor_t *sensor) {
 	// get error thresholds
-    int32_t reading = sensor->sampleSensor(sensor);
-    int32_t upperThres = sensor->maxReading + cmr_getReadingTolerance(sensor);
-    int32_t lowerThres = sensor->minReading - cmr_getReadingTolerance(sensor);
+    uint32_t reading = sensor->sampleSensor(sensor);
+    uint32_t upperThres = sensor->maxReading + cmr_getReadingTolerance(sensor);
+    uint32_t lowerThres = sensor->minReading - cmr_getReadingTolerance(sensor);
 
 	// set error
-    if ((upperThres >= sensor->maxReading && reading > upperThres) ||
-        (lowerThres <= sensor->minReading && reading < lowerThres)) {
-
+    if (
+        (upperThres >= sensor->maxReading && reading > upperThres) ||
+        (lowerThres <= sensor->minReading && reading < lowerThres)
+    ) {
         sensor->error = SENSOR_ERR_OUT_OF_RANGE;
     } else {
         sensor->error = SENSOR_ERR_NONE;
@@ -63,3 +47,4 @@ void cmr_getSensorValue(cmr_sensor_t* sensor) {
 	// convert reading to value
     sensor->value = sensor->readingToValue(sensor, reading);
 }
+
--- CMR/sensors.h
+++ CMR/sensors.h
@@ -12,23 +12,26 @@
 #include <CMR/can_types.h>
 #include <stdbool.h>
 
+/** @brief Sensor errors. */
 typedef enum {
-    SENSOR_ERR_NONE = 0,
-    SENSOR_ERR_OUT_OF_RANGE,
-    SENSOR_ERR_LEN
+    SENSOR_ERR_NONE = 0,        /**< @brief No errors. */
+    SENSOR_ERR_OUT_OF_RANGE,    /**< @brief Sensor out of range. */
+    SENSOR_ERR_LEN              /**< @brief Number of sensor errors. */
 } cmr_sensorError_t;
 
+/** @brief Represents a sensor. */
 typedef struct cmr_sensor cmr_sensor_t;
 
 /**
  * @brief Function pointer typedef for converting sensor's raw data
  * value to a real value.
  */
-typedef int32_t (*cmr_readingToValue_t) (cmr_sensor_t *, int32_t);
+typedef int32_t (*cmr_readingToValue_t) (cmr_sensor_t *, uint32_t);
+
 /**
  * @brief Function pointer typedef for getting a raw reading from a sensor.
  */
-typedef int32_t (*cmr_sampleSensor_t) (cmr_sensor_t *);
+typedef uint32_t (*cmr_sampleSensor_t) (cmr_sensor_t *);
 
 /**
  * @brief Function pointer typedef for initializing a sensor.
@@ -37,21 +40,19 @@ typedef void (*cmr_initSensor_t) (cmr_sensor_t *);
 
 struct cmr_sensor {
     const cmr_readingToValue_t readingToValue; /**< @brief Function pointer for sensor reading to value. */
-    const cmr_initSensor_t sensorInit; 	       /**< @brief Function pointer for initializing sensor. */
     const cmr_sampleSensor_t sampleSensor;     /**< @brief Function pointer for Sensor reading to value conversion . */
 
-    bool isInitialized;	                       /**< @brief Whether the sensor is initialized or not. */
-
     uint32_t minReading;                       /**< @brief Minimum expected sensor reading. */
-    uint32_t maxReading;                       /**< @brief Maximum expected sensor readinge. */
+    uint32_t maxReading;                       /**< @brief Maximum expected sensor reading. */
     uint32_t warnThres_pcnt;                   /**< @brief Out-of-range error threshold percentage. */
-    cmr_canWarn_t flag;                        /**< @brief Heartbeat flag to set when out of range. */
+    cmr_canWarn_t warnFlag;                    /**< @brief Heartbeat warning flag to set when out of range. */
+    cmr_canError_t errorFlag;                  /**< @brief Heartbeat error flag to set when out of range. */
 
     volatile cmr_sensorError_t error;          /**< @brief Sensor error status. */
     volatile int32_t value;                    /**< @brief Current value in proper units. */
 };
 
 void cmr_getSensorValue(cmr_sensor_t *);
-void cmr_initializeSensor(cmr_sensor_t *);
 
 #endif /* CMR_SENSORS_H */
+
