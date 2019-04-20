b5c340d744879cd355efd246eb1c03d6abfff235
CMR Treasurer <48072198+CMR-treasurer@users.noreply.github.com>
2019-04-20 12:38:20 -0400

    Update startup image and DL conversion script

--- DIM-ESE/ese_dl.py
+++ DIM-ESE/ese_dl.py
@@ -1,29 +1,25 @@
-# Paste RAM_DL text from EVE Screen Editor after the prompt
+#!/usr/bin/env python3
+
+# Paste RAM_DL text from EVE Screen Editor.
 # (It can be found in the 'Inspector' tab at the bottom left)
-# Ensure the pasted text has line breaks after each command
-# Ensure that the last line is a DISPLAY() cmd, perferably the first one
-# Press CTRL-D to terminate Python shell to get output
+# Ensure the pasted text has line breaks after each command.
+# Ensure that the last line is a DISPLAY() cmd; preferably the first one.
+# Press CTRL-D to indicate end-of-file.
 
 import sys
-print ('Paste stuff here:')
 
-dl = sys.stdin.read()
+# Write display list start command.
+sys.stdout.write("0xffffff00,\n")
 
-def function(dl):
-    dl = dl.split("\n")
-    new_dl =[]
-    for x in dl:
-        i = x.find("0x",0,14)
-        if (i == -1):
-             pass
-        else:
-            s = x[i:i+10]
-            new_dl.append(s+",\n")
-    new_dl = ''.join(new_dl)
-    return new_dl
-    
-print("\nHere's your display list :)\n")
+# Parse input.
+for line in sys.stdin:
+    i = line.find("0x")
+    if (i < 0):
+        continue
 
-new_dl = function(dl)
-new_dl = '\n'.join(["0xffffff00,",new_dl])
-print(new_dl + '0xffffff01')
+    # Print display list value.
+    sys.stdout.write(line[i:i + 10] + ",\n")
+
+# Write display list end command.
+sys.stdout.write('0xffffff01,')
+
