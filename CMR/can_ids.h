603a0ebaebc9f392a05cd40117821149cefca34d
Zachary B Pomper <zbp@andrew.cmu.edu>
2019-12-31 19:48:06 -0500

    More daq support

--- CMR/can_ids.h
+++ CMR/can_ids.h
@@ -60,6 +60,7 @@ typedef enum {
 
     CMR_CANID_DAQ_SENSORS0_ADC = 0x2AC,          /**< @brief DAQ sensor data 0. */
     CMR_CANID_DAQ_SENSORS1_ADC = 0x2BC,          /**< @brief DAQ sensor data 1. */
+    CMR_CANID_DAQ_POST = 0x2BD,                  /**< @brief DAQ startup message */
 
     CMR_CANID_DIM_REQUEST = 0x235,              /**< @brief DIM state/gear request. */
     CMR_CANID_DIM_POWER_DIAGNOSTICS = 0x535,    /**< @brief DIM power diagnostics. */
