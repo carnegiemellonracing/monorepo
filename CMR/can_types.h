9a16330858e273e0c4a5a48eb5c69852ec587e68
Deepak Pallerla <dpallerl@andrew.cmu.edu>
2020-02-07 14:32:05 -0500

    Fix indentation

--- CMR/can_types.h
+++ CMR/can_types.h
@@ -539,22 +539,22 @@ typedef struct {
 
 /** @brief AMK motor controller status bits. */
 typedef enum {
-    CMR_CAN_AMK_STATUS_SYSTEM_READY = (1 << 8), /**< @brief System ready. */
-    CMR_CAN_AMK_STATUS_ERROR = (1 << 9),        /**< @brief Error is present. */
-    CMR_CAN_AMK_STATUS_WARNING = (1 << 10),     /**< @brief Warning is present. */
-    CMR_CAN_AMK_STATUS_HV_EN_ACK = (1 << 11),   /**< @brief HV enabled acknowledgement. */
-    CMR_CAN_AMK_STATUS_HV_EN = (1 << 12),       /**< @brief HV enabled. */
-    CMR_CAN_AMK_STATUS_INV_EN_ACK = (1 << 13),  /**< @brief Inverter enabled acknowledgement. */
-    CMR_CAN_AMK_STATUS_INV_EN = (1 << 14),      /**< @brief Inverter enabled. */
-    CMR_CAN_AMK_STATUS_DERATING_EN = (1 << 15)  /**< @brief Protective torque derating enabled. */
+    CMR_CAN_AMK_STATUS_SYSTEM_READY = (1 << 8),     /**< @brief System ready. */
+    CMR_CAN_AMK_STATUS_ERROR        = (1 << 9),     /**< @brief Error is present. */
+    CMR_CAN_AMK_STATUS_WARNING      = (1 << 10),    /**< @brief Warning is present. */
+    CMR_CAN_AMK_STATUS_HV_EN_ACK    = (1 << 11),    /**< @brief HV enabled acknowledgement. */
+    CMR_CAN_AMK_STATUS_HV_EN        = (1 << 12),    /**< @brief HV enabled. */
+    CMR_CAN_AMK_STATUS_INV_EN_ACK   = (1 << 13),    /**< @brief Inverter enabled acknowledgement. */
+    CMR_CAN_AMK_STATUS_INV_EN       = (1 << 14),    /**< @brief Inverter enabled. */
+    CMR_CAN_AMK_STATUS_DERATING_EN  = (1 << 15)     /**< @brief Protective torque derating enabled. */
 } cmr_canAMKStatus_t;
 
 /** @brief AMK motor controller control bits. */
 typedef enum {
-    CMR_CAN_AMK_CTRL_INV_ON = (1 << 8),     /**< @brief Inverter on command. */
-    CMR_CAN_AMK_CTRL_HV_EN = (1 << 9),      /**< @brief HV enable command. */
-    CMR_CAN_AMK_CTRL_INV_EN = (1 << 10),    /**< @brief Inverter enable command. */
-    CMR_CAN_AMK_CTRL_ERR_RESET = (1 << 11)  /**< @brief Inverter error reset command. */
+    CMR_CAN_AMK_CTRL_INV_ON     = (1 << 8),     /**< @brief Inverter on command. */
+    CMR_CAN_AMK_CTRL_HV_EN      = (1 << 9),     /**< @brief HV enable command. */
+    CMR_CAN_AMK_CTRL_INV_EN     = (1 << 10),    /**< @brief Inverter enable command. */
+    CMR_CAN_AMK_CTRL_ERR_RESET  = (1 << 11)     /**< @brief Inverter error reset command. */
 } cmr_canAMKControl_t;
 
 /** @brief AMK motor controller status and velocity. */
