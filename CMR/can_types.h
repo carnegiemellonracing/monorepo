85eb32017e6f3aad7dbd8465154ccebd014508ef
Omkar Savkur <osavkur@andrew.cmu.edu>
2022-06-28 21:28:27 -0400

    Updated Hall Effect to centi-amps

--- CMR/can_types.h
+++ CMR/can_types.h
@@ -262,7 +262,7 @@ typedef struct {
 /** @brief Vehicle Safety Module sensor data. */
 typedef struct {
     uint16_t brakePressureRear_PSI;     /**< @brief Rear brake pressure (pounds-per-square-inch). */
-    int16_t hallEffect_mA;     /**< @brief Hall effect current (milli-Amps). */
+    int16_t hallEffect_cA;     /**< @brief Hall effect current (centi-Amps). */
     uint8_t safetyIn_dV;        /**< @brief Safety circuit input voltage (deci-Volts). */
     uint8_t safetyOut_dV;       /**< @brief Safety circuit output voltage (deci-Volts). */
 } cmr_canVSMSensors_t;
