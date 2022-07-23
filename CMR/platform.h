85f840f2539da9578664e24fa373bb56a2de5851
Omkar Savkur <osavkur@andrew.cmu.edu>
2022-07-23 16:07:03 -0400

    Fixed platform.h to get order of preprocessor operations correct

--- CMR/platform.h
+++ CMR/platform.h
@@ -5,14 +5,18 @@
 #define L431_COMPARE    1
 #define H735_COMPARE    2
 
-#define COMPARE_PROCESSOR(processor)    (processor ## _COMPARE)
+#define CONCAT_PROCESSOR(platform)    (platform ## _COMPARE)
+#define COMPARE_PROCESSOR(platform)    CONCAT_PROCESSOR(platform)
 
-#if defined(PROCESSOR) && (COMPARE_PROCESSOR(PROCESSOR)==F413_COMPARE)
+#if defined(PLATFORM) && (COMPARE_PROCESSOR(PLATFORM)==F413_COMPARE)
 #include <stm32f4xx_hal.h>
-#elif defined(PROCESSOR) && (COMPARE_PROCESSOR(PROCESSOR)==L431_COMPARE)
+#elif defined(PLATFORM) && (COMPARE_PROCESSOR(PLATFORM)==L431_COMPARE)
 #include <stm32l4xx_hal.h>
-#elif defined(PROCESSOR) && (COMPARE_PROCESSOR(PROCESSOR)==H735_COMPARE)
+#elif defined(PLATFORM) && (COMPARE_PROCESSOR(PLATFORM)==H735_COMPARE)
 #include <stm32h7xx_hal.h>
 #endif
+
+#undef CONCAT_PROCESSOR
+#undef COMPARE_PROCESSOR
 
 #endif /* PLATFORM_H */
