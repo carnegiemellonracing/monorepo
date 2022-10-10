a8f66cbb7ff3eab2da9a86a66fc68758e4f0e3f1
kusti8 <hello@kusti8.com>
2022-10-10 13:35:39 -0400

    Add initial i2c draft

---
+++ i2c.c
@@ -0,0 +1,43 @@
+/*
+ * i2c.c
+ *
+*/
+
+#include "i2c.h"
+
+void i2cInit(void) {
+    cmr_i2cInit(&bmb_i2c, I2C1,
+                100000, 0, // 100kHz limited by the PCA9536 TODO: Check if own address should be 0
+                GPIOB, GPIO_PIN_8, // clock
+                GPIOB, GPIO_PIN_9); // data
+}
+
+// This verifies everything looks like it works
+// and configures everything
+// A non-zero return value indicates error, higher 8 bits
+// indicates the mux that produced the error, then lower 8
+// bits 0 indicates mux failure, 1 indicates 4 IO expander
+// failure, 2 indicates ADC failure, 3 indicates 9 IO expander
+// failure
+uint16_t i2cVerifyConfigChain(void) {
+    for (uint8_t i = 0; i < NUM_BMBS; i++) {
+        uint8_t data = 0;
+        if (cmr_i2cRX(&bmb_i2c, bms_mux_address[i], &data,
+                  1, I2C_TIMEOUT) != 0) {
+            return ((uint16_t)(i)) << 8;
+        }
+        if (data != 0x0) {
+            return ((uint16_t)(i)) << 8;
+        }
+        // verified mux is reset to 0
+        // now enable the mux, select the 4 IO expander
+        data = 0x04; // selects channel 0 (0x05 is channel 1)
+        if (cmr_i2cTX(&bmb_i2c, bms_mux_address[i], &data,
+                  1, I2C_TIMEOUT) != 0) {
+            return ((uint16_t)(i)) << 8;
+        }
+        // now verify the 4 IO expander
+        if (
+    }
+    return 0;
+}
---
+++ i2c.h
@@ -0,0 +1,27 @@
+/*
+ * i2c.h
+ * Header for I2C interface to BMB I2C muxes, GPIO expanders, and ADCs
+ * Originally written by Gustav Hanse (gustavh)
+ * Edited on Sep 29, 2022
+ *
+*/
+
+#ifndef I2C_H
+#define I2C_H
+
+#include <CMR/i2c.h>
+
+#define NUM_BMBS 8
+// I2c addresses of muxes 
+// From https://www.nxp.com/docs/en/data-sheet/PCA9542A.pdf
+static const uint16_t bms_mux_address[NUM_BMBS] = {
+    0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77
+};
+
+#define BMS_SELECT_IO_ADDR 0x41;
+#define BMS_ADC_ADDR 0x33;
+#define BMS_CELL_BALANCE_IO_ADDR 0x50; // TODO: Change this, variable from 0x50-0x57
+
+static cmr_i2c_t bmb_i2c;
+
+#endif
