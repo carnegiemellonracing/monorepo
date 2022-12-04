3135410675c3e7111c4032f77f07f67bf9b940e1
Trent Muskens <tmuskens@andrew.cmu.edu>
2022-12-04 10:20:03 -0500

    added semicolon

--- CMR/can_types.h
+++ CMR/can_types.h
@@ -538,7 +538,7 @@ typedef struct {
     int16_t angle;
     int16_t pwm_left;
     int16_t pwm_right;
-} cmr_canCDCDRSStates_t
+} cmr_canCDCDRSStates_t;
 
 // ------------------------------------------------------------------------------------------------
 // Central Dynamics Controller (20e)
