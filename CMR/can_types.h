10c3db2f4d030a8e4cdfc9c604e50adb4a3b2b50
Liam Merino <35940408+MerinoSheep@users.noreply.github.com>
2023-12-13 14:19:04 -0600

    packed button status

--- CMR/can_types.h
+++ CMR/can_types.h
@@ -664,14 +664,7 @@ typedef struct {
 } cmr_canDIMTextWrite_t;
 
 typedef struct {
-    uint8_t action0ButtonPressed;    /**< @brief Status of the action 0 button (Active Low). */
-    uint8_t action1ButtonPressed;    /**< @brief Status of the action 1 button (Active Low). */
-    uint8_t action2ButtonPressed;    /**< @brief Status of the action 2 button (Active Low). */
-    uint8_t drsButtonPressed;        /**< @brief Status of the AE/DRS button (Active Low). */
-    uint8_t actionUpButtonPressed;   /** @brief Status of the action up button. */
-    uint8_t actionDownButtonPressed; /** @brief Status of the action down button. */
-    uint8_t actionLeftButtonPressed; /** @brief Status of the action left button. */
-    uint8_t actionRightButtonPressed;/** @brief Status of the action right button. */
+    uint8_t buttons;                 /**< @brief Button states packed into an uint8_t. {drs,0,1,2,up,down,left,right}*/
     uint8_t regenPercent;            /**< @brief Integer percentage for regen. */
     uint8_t paddleLeft;              /**< @brief Between 0 and 255 for left paddle pos*/
     uint8_t paddleRight;             /**< @brief Between 0 and 255 for left paddle pos*/
