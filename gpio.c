a7df99f5df0a0b30b7a9755b52697b96f2aaab7d
FMS Computer 2 <cmr.treasurer@gmail.com>
2024-04-16 03:39:47 -0400

    fixed ordering of DIM error lights

--- gpio.c
+++ gpio.c
@@ -33,8 +33,8 @@ static const cmr_gpioPinConfig_t gpioPinConfigs[GPIO_LEN] = {
             .Speed = GPIO_SPEED_FREQ_LOW } },
     [GPIO_LED_1] = { .port = GPIOB, .init = { .Pin = GPIO_PIN_5, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW } },
     [GPIO_LED_2] = { .port = GPIOB, .init = { .Pin = GPIO_PIN_4, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW } },  // not in the schematic
-    [GPIO_LED_AMS] = { .port = GPIOA, .init = { .Pin = GPIO_PIN_11, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW } },
-    [GPIO_LED_IMD] = { .port = GPIOA, .init = { .Pin = GPIO_PIN_9, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW } },
+    [GPIO_LED_AMS] = { .port = GPIOA, .init = { .Pin = GPIO_PIN_9, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW } },
+    [GPIO_LED_IMD] = { .port = GPIOA, .init = { .Pin = GPIO_PIN_11, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW } },
     [GPIO_LED_BSPD] = { .port = GPIOA, .init = { .Pin = GPIO_PIN_10, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW } },
     [GPIO_PTT_N] = { .port = GPIOC, .init = { .Pin = GPIO_PIN_8, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW } },
     [GPIO_PD_N] = { .port = GPIOA, .init = { .Pin = GPIO_PIN_5, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW } }
