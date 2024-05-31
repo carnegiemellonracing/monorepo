4dc8bfd7f303bcf7375ff4906b7b9bab5f7f7ff3
Liam Merino <35940408+MerinoSheep@users.noreply.github.com>
2024-05-30 21:44:00 -0500

    initial commit, temporary bloat in freertos, restructure of CMR drivers to have src and incldue

---
+++ CMR/src/qspi.c
@@ -0,0 +1,285 @@
+/**
+ * @file qspi.c
+ * @brief QuadSPI implementation.
+ *
+ * The `HAL_QSPI_...Callback()` functions override weak implementations in the
+ * HAL SPI driver (`stm32f4xx_hal_spi.c`).
+ *
+ * @warning All callbacks are called from interrupt handlers!
+ *
+ * @author Carnegie Mellon Racing
+ */
+
+#include "qspi.h"   // Interface to implement
+#include "panic.h"
+
+#ifdef HAL_QSPI_MODULE_ENABLED
+
+#include "dma.h"    // DMA interface
+#include "rcc.h"    // cmr_rccQSPIClockEnable(), cmr_rccGPIOClockEnable()
+
+/** @brief QuadSPI device configuration. */
+typedef struct {
+    /** @brief The associated handle, or `NULL` if not configured. */
+    QSPI_HandleTypeDef *handle;
+} cmr_qspiDevice_t;
+
+/**
+ * @brief All QuadSPI device configurations, indexed by interface.
+ *
+ * There is only 1 QuadSPI interface on the STM32F413.
+ */
+static cmr_qspiDevice_t cmr_qspiDevices[1];
+
+/**
+ * @brief QuadSPI interrupt handler.
+ */
+void QUADSPI_IRQHandler(void) {
+    HAL_QSPI_IRQHandler(cmr_qspiDevices[0].handle);
+}
+
+/**
+ * @brief Handles QuadSPI completion for the given port.
+ *
+ * @warning The handle must have been configured through this library!
+ *
+ * @param handle The HAL QuadSPI handle.
+ */
+static void cmr_qspiDoneCallback(QSPI_HandleTypeDef *handle) {
+    char *addr = (void *) handle;
+    cmr_qspi_t *qspi = (void *) (addr - offsetof(cmr_qspi_t, handle));
+
+    // Indicate completion.
+    BaseType_t higherWoken;
+    if (xSemaphoreGiveFromISR(qspi->doneSem, &higherWoken) != pdTRUE) {
+        cmr_panic("QuadSPI done semaphore released more than once!");
+    }
+    portYIELD_FROM_ISR(higherWoken);
+}
+
+/**
+ * @brief HAL QuadSPI command completion handler.
+ *
+ * @param handle The HAL UART handle.
+ */
+void HAL_QSPI_CmdCpltCallback(QSPI_HandleTypeDef *handle) {
+    cmr_qspiDoneCallback(handle);
+}
+
+/**
+ * @brief HAL QuadSPI transmit completion handler.
+ *
+ * @param handle The HAL UART handle.
+ */
+void HAL_QSPI_TxCpltCallback(QSPI_HandleTypeDef *handle) {
+    cmr_qspiDoneCallback(handle);
+}
+
+/**
+ * @brief HAL QuadSPI receive completion handler.
+ *
+ * @param handle The HAL UART handle.
+ */
+void HAL_QSPI_RxCpltCallback(QSPI_HandleTypeDef *handle) {
+    cmr_qspiDoneCallback(handle);
+}
+
+/**
+ * @brief Initializes QuadSPI.
+ *
+ * @warning It is undefined behavior to initialize the same HAL QSPI or DMA
+ * stream instances more than once!
+ *
+ * @param qspi The QuadSPI port to initialize.
+ * @param instance The HAL QUADSPI instance (`QUADSPI` from `stm32f413xx.h`).
+ * @param init The HAL QSPI configuration (see `stm32f4xx_hal_spi.h`).
+ * @param pins Pin configuration.
+ * @param dma HAL DMA stream instance (`DMAx_StreamN` from `stm32f413xx.h`).
+ * @param dmaChannel HAL DMA channel (`DMA_CHANNEL_x` from
+ * `stm32f4xx_hal_dma.h`).
+ */
+void cmr_qspiInit(
+    cmr_qspi_t *qspi, QUADSPI_TypeDef *instance, const QSPI_InitTypeDef *init,
+    const cmr_qspiPinConfig_t *pins,
+    DMA_Stream_TypeDef *dma, uint32_t dmaChannel
+) {
+    *qspi = (cmr_qspi_t) {
+        .handle = {
+            .Instance = instance,
+            .Init = *init
+        },
+        .dma = {
+            .Instance = dma,
+            .Init = {
+                .Channel = dmaChannel,
+                .Direction = DMA_PERIPH_TO_MEMORY,
+                .PeriphInc = DMA_PINC_DISABLE,
+                .MemInc = DMA_MINC_ENABLE,
+                .PeriphDataAlignment = DMA_PDATAALIGN_BYTE,
+                .MemDataAlignment = DMA_MDATAALIGN_BYTE,
+                .Mode = DMA_NORMAL,
+                .Priority = DMA_PRIORITY_LOW,
+                .FIFOMode = DMA_FIFOMODE_DISABLE
+            }
+        },
+    };
+
+    qspi->doneSem = xSemaphoreCreateBinaryStatic(&qspi->doneSemBuf);
+    configASSERT(qspi->doneSem != NULL);
+
+    cmr_rccQSPIClockEnable(instance);
+
+    // Configure pins.
+    GPIO_InitTypeDef pinConfig = {
+        .Pin = pins->sck.pin,
+        .Mode = GPIO_MODE_AF_PP,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_VERY_HIGH,
+        .Alternate = GPIO_AF9_QSPI
+    };
+    cmr_rccGPIOClockEnable(pins->sck.port);
+    HAL_GPIO_Init(pins->sck.port, &pinConfig);
+
+    pinConfig.Pin = pins->nss.pin;
+    cmr_rccGPIOClockEnable(pins->nss.port);
+    HAL_GPIO_Init(pins->nss.port, &pinConfig);
+
+    // All I/O pins are AF10.
+    pinConfig.Alternate = GPIO_AF10_OTG_FS;
+    for (size_t i = 0; i < sizeof(pins->io) / sizeof(pins->io[0]); i++) {
+        const cmr_qspiPin_t *pin = pins->io + i;
+        pinConfig.Pin = pin->pin;
+        cmr_rccGPIOClockEnable(pin->port);
+        HAL_GPIO_Init(pin->port, &pinConfig);
+    }
+
+    // Configure DMA.
+    cmr_dmaInit(&qspi->dma);
+    __HAL_LINKDMA(&qspi->handle, hdma, qspi->dma);
+
+    // Configure device.
+    cmr_qspiDevices[0].handle = &qspi->handle;
+    HAL_NVIC_SetPriority(QUADSPI_IRQn, 7, 0);
+    HAL_NVIC_EnableIRQ(QUADSPI_IRQn);
+
+    if (HAL_QSPI_Init(&qspi->handle) != HAL_OK) {
+        cmr_panic("HAL_QSPI_Init() failed!");
+    }
+}
+
+/**
+ * @brief Sets the clock prescaler.
+ *
+ * @param qspi The QuadSPI port.
+ * @param prescaler The new prescaler.
+ */
+void cmr_qspiSetPrescaler(cmr_qspi_t *qspi, uint32_t prescaler) {
+    uint32_t qspiCR = (qspi->handle.Instance->CR) & ((1 << 24) - 1);
+    qspi->handle.Instance->CR = qspiCR | (prescaler << 24);
+}
+
+/**
+ * @brief Sends a command that has no data.
+ *
+ * @note Blocks until the transaction actually completes.
+ * @note Does NOT block if the port is currently busy.
+ *
+ * @warning `cmd->DataMode` MUST be `QSPI_DATA_NONE`!
+ *
+ * @param qspi The QuadSPI port.
+ * @param cmd The command's configuration.
+ */
+void cmr_qspiCmd(cmr_qspi_t *qspi, const QSPI_CommandTypeDef *cmd) {
+    configASSERT(cmd->DataMode == QSPI_DATA_NONE);
+
+    // HAL does not declare the command pointer `const`, even though it doesn't
+    // modify it. Oh well.
+    if (
+        HAL_QSPI_Command_IT(&qspi->handle, (QSPI_CommandTypeDef *) cmd) !=
+        HAL_OK
+    ) {
+        cmr_panic("HAL_QSPI_Command_IT() failed!");
+    }
+
+    // Wait for transaction to complete.
+    if (xSemaphoreTake(qspi->doneSem, portMAX_DELAY) != pdTRUE) {
+        cmr_panic("Acquiring QuadSPI port done semaphore timed out!");
+    }
+}
+
+/**
+ * @brief Sends data.
+ *
+ * @note Blocks until the transaction actually completes.
+ * @note Does NOT block if the port is currently busy.
+ *
+ * @warning `cmd->DataMode` MUST NOT be `QSPI_DATA_NONE`!
+ * @warning `data` MUST have at least `cmd->NbData` bytes!
+ *
+ * @param qspi The QuadSPI port.
+ * @param cmd The command's configuration.
+ */
+void cmr_qspiTX(
+    cmr_qspi_t *qspi, const QSPI_CommandTypeDef *cmd, const void *data
+) {
+    configASSERT(cmd->DataMode != QSPI_DATA_NONE);
+
+    // HAL does not declare the command pointer `const`, even though it doesn't
+    // modify it. Oh well.
+    if (
+        HAL_QSPI_Command(&qspi->handle, (QSPI_CommandTypeDef *) cmd, 0) !=
+        HAL_OK
+    ) {
+        cmr_panic("HAL_QSPI_Command() failed!");
+    }
+
+    // HAL does not declare the data pointer `const`, even though it doesn't
+    // modify it. Oh well.
+    if (HAL_QSPI_Transmit_DMA(&qspi->handle, (void *) data) != HAL_OK) {
+        cmr_panic("HAL_QSPI_Transmit_DMA() failed!");
+    }
+
+    // Wait for transaction to complete.
+    if (xSemaphoreTake(qspi->doneSem, portMAX_DELAY) != pdTRUE) {
+        cmr_panic("Acquiring QuadSPI port done semaphore timed out!");
+    }
+}
+
+/**
+ * @brief Receives data.
+ *
+ * @note Blocks until the transaction actually completes.
+ * @note Does NOT block if the port is currently busy.
+ *
+ * @warning `cmd->DataMode` MUST NOT be `QSPI_DATA_NONE`!
+ * @warning `data` MUST have at least `cmd->NbData` bytes!
+ *
+ * @param qspi The QuadSPI port.
+ * @param cmd The command's configuration.
+ */
+void cmr_qspiRX(
+    cmr_qspi_t *qspi, const QSPI_CommandTypeDef *cmd, void *data
+) {
+    configASSERT(cmd->DataMode != QSPI_DATA_NONE);
+
+    // HAL does not declare the command pointer `const`, even though it doesn't
+    // modify it. Oh well.
+    if (
+        HAL_QSPI_Command(&qspi->handle, (QSPI_CommandTypeDef *) cmd, 0) !=
+        HAL_OK
+    ) {
+        cmr_panic("HAL_QSPI_Command() failed!");
+    }
+
+    if (HAL_QSPI_Receive_DMA(&qspi->handle, data) != HAL_OK) {
+        cmr_panic("HAL_QSPI_Receive_DMA() failed!");
+    }
+
+    // Wait for transaction to complete.
+    if (xSemaphoreTake(qspi->doneSem, portMAX_DELAY) != pdTRUE) {
+        cmr_panic("Acquiring QuadSPI port done semaphore timed out!");
+    }
+}
+
+#endif /* HAL_QSPI_MODULE_ENABLED */
+
---
+++ CMR/src/sensors.c
@@ -0,0 +1,172 @@
+/**
+ * @file sensors.c
+ * @brief Sensor reading and conversion implementation.
+ *
+ * @author Carnegie Mellon Racing
+ */
+
+#include <FreeRTOS.h>   // configASSERT()
+
+#include "sensors.h"     // Interface to implement
+
+/**
+ * @brief Initializes the sensor.
+ *
+ * @note The sensor's public fields should already be filled in!
+ *
+ * @param sensor The sensor to initialize.
+ */
+static void cmr_sensorInit(cmr_sensor_t *sensor) {
+    configASSERT(sensor->readingMax >= sensor->readingMin);
+
+    // Calculate tolerance.
+    const uint32_t readingRange = sensor->readingMax - sensor->readingMin;
+    uint32_t readingTolerance;
+    // Prevent overflow.
+    if (UINT32_MAX / sensor->outOfRange_pcnt < readingRange) {
+        readingTolerance = (readingRange / 100) * sensor->outOfRange_pcnt;
+    } else {
+        readingTolerance = (readingRange * sensor->outOfRange_pcnt) / 100;
+    }
+
+    // Calculate thresholds from tolerance.
+    uint32_t readingUpper = sensor->readingMax + readingTolerance;
+    if (readingUpper < sensor->readingMax) {
+        readingUpper = UINT32_MAX;  // Overflow!
+    }
+
+    uint32_t readingLower = sensor->readingMin - readingTolerance;
+    if (readingLower > sensor->readingMin) {
+        readingLower = 0;  // Underflow!
+    }
+
+    sensor->_ = (struct cmr_sensor_private) {
+        .readingUpper = readingUpper,
+        .readingLower = readingLower,
+        .value = 0,
+        .error = CMR_SENSOR_ERR_NONE
+    };
+}
+
+/**
+ * @brief Updates the sensor.
+ *
+ * @param sensor The sensor to sample.
+ */
+static void cmr_sensorUpdate(cmr_sensor_t *sensor) {
+    uint32_t reading = sensor->sample(sensor);
+
+    // Check if sensor has gone out-of-range.
+    if (reading < sensor->_.readingLower || sensor->_.readingUpper < reading) {
+        sensor->_.error = CMR_SENSOR_ERR_OUT_OF_RANGE;
+    } else {
+        sensor->_.error = CMR_SENSOR_ERR_NONE;
+    }
+
+    uint32_t value;
+    if (sensor->conv != NULL) {
+        value = sensor->conv(sensor, reading);
+    } else {
+        value = reading;
+    }
+
+    sensor->_.value = value;
+}
+
+/**
+ * @brief Initializes the sensor list.
+ *
+ * @param list The list to initialize.
+ * @param sensors The array of sensors in the list.
+ * @param sensorsLen The total number of sensors in the array.
+ */
+void cmr_sensorListInit(
+    cmr_sensorList_t *list,
+    cmr_sensor_t *sensors, size_t sensorsLen
+) {
+    for (size_t i = 0; i < sensorsLen; i++) {
+        cmr_sensorInit(sensors + i);
+    }
+
+    list->sensors = sensors;
+    list->sensorsLen = sensorsLen;
+}
+
+/**
+ * @brief Updates the given sensor list.
+ *
+ * @param list The list to update.
+ */
+void cmr_sensorListUpdate(cmr_sensorList_t *list) {
+    for (size_t i = 0; i < list->sensorsLen; i++) {
+        cmr_sensorUpdate(list->sensors + i);
+    }
+}
+
+/**
+ * @brief Sets the warning and/or error flags for each out-of-range sensor.
+ *
+ * @note The flags are bitwise-OR'd into the provided fields.
+ *
+ * @param list The list.
+ * @param warnp The warnings to update, or NULL to ignore.
+ * @param errorp The errors to update, or NULL to ignore.
+ */
+void cmr_sensorListGetFlags(
+    cmr_sensorList_t *list,
+    cmr_canWarn_t *warnp, cmr_canError_t *errorp
+) {
+    cmr_canWarn_t warn = (warnp == NULL) ? 0 : *warnp;
+    cmr_canError_t error = (errorp == NULL) ? 0 : *errorp;
+
+    for (size_t i = 0; i < list->sensorsLen; i++) {
+        cmr_sensor_t *sensor = list->sensors + i;
+        switch (sensor->_.error) {
+            case CMR_SENSOR_ERR_OUT_OF_RANGE:
+                warn |= sensor->warnFlag;
+                error |= sensor->errorFlag;
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (warnp != NULL) {
+        *warnp |= warn;
+    }
+
+    if (errorp != NULL) {
+        *errorp |= error;
+    }
+}
+
+/**
+ * @brief Gets the most recent converted value for the given channel.
+ *
+ * @param list The list to use.
+ * @param channel The channel in the list (i.e., the index of the sensor in the
+ * original array passed to `cmr_sensorListInit()`).
+ *
+ * @return The most recent converted value.
+ */
+int32_t cmr_sensorListGetValue(
+    cmr_sensorList_t *list, size_t channel
+) {
+    return list->sensors[channel]._.value;
+}
+
+/**
+ * @brief Gets the most recent error for the given channel.
+ *
+ * @param list The list to use.
+ * @param channel The channel in the list (i.e., the index of the sensor in the
+ * original array passed to `cmr_sensorListInit()`).
+ *
+ * @return The most recent error.
+ */
+cmr_sensorError_t cmr_sensorListGetError(
+    cmr_sensorList_t *list, size_t channel
+) {
+    return list->sensors[channel]._.error;
+}
+
