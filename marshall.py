6d684bba536ff14bda4fe9cd5798c8505c61782f
Liam Merino <35940408+MerinoSheep@users.noreply.github.com>
2024-06-23 22:11:00 -0500

    linker and cmake-dim drivers

--- marshall.py
+++ marshall.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 path = "can_fmt.rawh"
-with open('can_fmt.json', 'r') as file_in, open ('Gen/can_fmt.rawh', 'w+') as file_out:
+with open('can_fmt.json', 'r') as file_in, open ('Src/can_fmt.rawh', 'w+') as file_out:
     data = file_in.read().replace('\n', '').replace('\"','\\\"').replace(' ','')
     data = '"' + data + '"'
     file_out.write(data)
