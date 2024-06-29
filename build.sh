a7915df23cb0a40f8bdcbb5b166ae030f112aace
root <root@ip-172-31-28-111.ec2.internal>
2024-06-29 23:24:35 +0000

    edited build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 BASEDIR=$(cd $(dirname $0) && pwd)
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE="${BASEDIR}\cmake\gcc-arm-none-eabi.cmake" -SC:"${BASEDIR}" -BC:"${BASEDIR}\build\debug" -G Ninja
-cmake --build "${BASEDIR}\build\debug" --target RAM --
+cmake=/usr/bin/cmake/bin/cmake
+$cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE="${BASEDIR}/cmake/gcc-arm-none-eabi.cmake" -S"${BASEDIR}" -B"${BASEDIR}/build/debug" -G Ninja
+$cmake --build "${BASEDIR}/build/debug" --target RAM --
