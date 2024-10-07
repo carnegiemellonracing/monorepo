8f09a8f29cb5edc8c48bec804b6c556d90fc06c8
Liam Merino <35940408+MerinoSheep@users.noreply.github.com>
2024-10-06 22:30:37 -0400

    formatting

--- CMR/src/tasks.c
+++ CMR/src/tasks.c
@@ -38,7 +38,7 @@ void cmr_taskInit(
         func,
         name,
         configMINIMAL_STACK_SIZE
-        arg,
+            arg,
         priority
     );
 #else
@@ -70,59 +70,58 @@ TaskHandle_t cmr_taskHandle(const cmr_task_t *task) {
 
 #if (configSUPPORT_STATIC_ALLOCATION)
 
-/**
- * @brief Provides memory used by the FreeRTOS idle task.
- *
- * @see https://www.freertos.org/a00110.html#configSUPPORT_STATIC_ALLOCATION
- *
- * @param task[out] The task state buffer.
- * @param stack[out] The stack buffer.
- * @param stackLen[out] The stack buffer's length, in words (NOT bytes).
- */
-void vApplicationGetIdleTaskMemory(
-    StaticTask_t **task,
-    StackType_t **stack,
-    configSTACK_DEPTH_TYPE *stackLen
-) {
-    /** @brief The task state buffer. */
-    static StaticTask_t taskBuf;
+    /**
+     * @brief Provides memory used by the FreeRTOS idle task.
+     *
+     * @see https://www.freertos.org/a00110.html#configSUPPORT_STATIC_ALLOCATION
+     *
+     * @param task[out] The task state buffer.
+     * @param stack[out] The stack buffer.
+     * @param stackLen[out] The stack buffer's length, in words (NOT bytes).
+     */
+    void vApplicationGetIdleTaskMemory(
+        StaticTask_t **task,
+        StackType_t **stack,
+        configSTACK_DEPTH_TYPE *stackLen
+    ) {
+        /** @brief The task state buffer. */
+        static StaticTask_t taskBuf;
 
-    /** @brief The task's stack buffer. */
-    static StackType_t stackBuf[configMINIMAL_STACK_SIZE];
+        /** @brief The task's stack buffer. */
+        static StackType_t stackBuf[configMINIMAL_STACK_SIZE];
 
-    *task = &taskBuf;
-    *stack = stackBuf;
-    *stackLen = sizeof(stackBuf) / sizeof(stackBuf[0]);
-}
+        *task = &taskBuf;
+        *stack = stackBuf;
+        *stackLen = sizeof(stackBuf) / sizeof(stackBuf[0]);
+    }
 
-#if (configUSE_TIMERS)
+    #if (configUSE_TIMERS)
 
-/**
- * @brief Provides memory used by the FreeRTOS timer task.
- *
- * @see https://www.freertos.org/a00110.html#configSUPPORT_STATIC_ALLOCATION
- *
- * @param task[out] The task state buffer.
- * @param stack[out] The stack buffer.
- * @param stackLen[out] The stack buffer's length, in words (NOT bytes).
- */
-void vApplicationGetTimerTaskMemory(
-    StaticTask_t **task,
-    StackType_t **stack,
-    uint32_t *stackLen
-) {
-    /** @brief The task state buffer. */
-    static StaticTask_t taskBuf;
+        /**
+         * @brief Provides memory used by the FreeRTOS timer task.
+         *
+         * @see https://www.freertos.org/a00110.html#configSUPPORT_STATIC_ALLOCATION
+         *
+         * @param task[out] The task state buffer.
+         * @param stack[out] The stack buffer.
+         * @param stackLen[out] The stack buffer's length, in words (NOT bytes).
+         */
+        void vApplicationGetTimerTaskMemory(
+            StaticTask_t **task,
+            StackType_t **stack,
+            uint32_t *stackLen
+        ) {
+            /** @brief The task state buffer. */
+            static StaticTask_t taskBuf;
 
-    /** @brief The task's stack buffer. */
-    static StackType_t stackBuf[configMINIMAL_STACK_SIZE];
+            /** @brief The task's stack buffer. */
+            static StackType_t stackBuf[configMINIMAL_STACK_SIZE];
 
-    *task = &taskBuf;
-    *stack = stackBuf;
-    *stackLen = sizeof(stackBuf) / sizeof(stackBuf[0]);
-}
+            *task = &taskBuf;
+            *stack = stackBuf;
+            *stackLen = sizeof(stackBuf) / sizeof(stackBuf[0]);
+        }
 
-#endif /* configUSE_TIMERS */
+    #endif /* configUSE_TIMERS */
 
 #endif /* configSUPPORT_STATIC_ALLOCATION */
-
