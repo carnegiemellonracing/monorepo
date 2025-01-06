e2a42c0bcf8733d593c340f8106c6eb9b08760cd
sidsrivastava001 <sidsrivastava001@gmail.com>
2025-01-06 13:41:40 -0500

    added fdcan and adc

--- stm32f413-drivers/CMR/src/adc.c
+++ stm32f413-drivers/CMR/src/adc.c
@@ -51,7 +51,12 @@ static void cmr_adcConfigChannels(cmr_adc_t *adc) {
         }
 
         // Rank goes from 1 to 16
-        ADC_ChannelConfTypeDef channelConfig = _platform_adcChannelConfig(channel, (uint32_t) i+1);
+        ADC_ChannelConfTypeDef channelConfig;
+#ifdef H725
+        channelConfig = _platform_adcChannelConfig(channel, (uint32_t) 6*(i+1));
+#else
+        channelConfig = _platform_adcChannelConfig(channel, (uint32_t) (i+1));
+#endif
 
         if (HAL_ADC_ConfigChannel(&adc->handle, &channelConfig) != HAL_OK) {
             cmr_panic("HAL_ADC_ConfigChannel() failed!");
