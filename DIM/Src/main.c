f0fabcb1558b5aa2244df6db9f65816965784d57
jeffreyshen <jshen3@andrew.cmu.edu>
2025-02-01 22:46:45 -0500

    my life is getting dimmer and dimmer with every passing day.

--- DIM/Src/main.c
+++ DIM/Src/main.c
@@ -112,11 +112,12 @@ static void errorLEDs(void *pvParameters) {
  */
 int main(void) {
     // System initialization.
-    HAL_Init();
+     HAL_Init();
+    /*
     cmr_rccSystemClockEnable();
 
     // Peripheral configuration.
-    gpioInit();
+    gpioInit(); 
     canInit();
     adcInit();
     stateMachineInit();
@@ -139,4 +140,5 @@ int main(void) {
 
     vTaskStartScheduler();
     cmr_panic("vTaskStartScheduler returned!");
+    */
 }
