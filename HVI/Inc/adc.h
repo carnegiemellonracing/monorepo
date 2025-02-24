9b7ad71c765f26670e6ac11e38c520c9d191371f
nidhivadlamudi <89157007+nidhivadlamudi@users.noreply.github.com>
2025-02-23 22:59:47 -0500

    added HVI (#30)
    * added HVI
    
    * updated gitignore
    
    * fixed issues
    
    * updated src files

---
+++ HVI/Inc/adc.h
@@ -0,0 +1,33 @@
+/**
+ * @file adc.h
+ * @brief Board-specific ADC interface.
+ *
+ * @author Carnegie Mellon Racing
+ */
+
+#ifndef ADC_H
+#define ADC_H
+
+#include <CMR/adc.h>    // ADC interface
+
+/**
+ * @brief Represents an ADC channel.
+ *
+ * @warning New channels MUST be added before `ADC_LEN`.
+ */
+typedef enum {
+	ADC_VSENSE,
+	ADC_ISENSE,
+	ADC_VREF,
+	ADC_LEN     /**< @brief Total ADC channels. */
+} adcChannels_t;
+
+
+extern cmr_adcChannel_t adcChannels[ADC_LEN];
+
+void adcInit(void);
+
+uint32_t adcRead(adcChannels_t ch);
+
+#endif /* ADC_H */
+
