5eec63ef989c818c31ef5a5042065b2ec4f34cb4
sid srivastava <49328871+sidsrivastava001@users.noreply.github.com>
2025-04-15 22:04:26 -0400

    cell voltage correct (#52)

--- LV-BMS/Inc/data.h
+++ LV-BMS/Inc/data.h
@@ -12,7 +12,7 @@
 
 // Converts float to uint16 (scale by 100)
 #define float_to_uint16(value) ((uint16_t)((value) * 100.0f))
-#define VREF_NOM 1.5  
+#define VREF_NOM 1.5 
 #define ADC_COUNT 4096L //assuming 12 bit adc
 #define GVCOUT 0.3 //(1.5V-->0.3)(3V-->0.6)
 #define GVSENSE 8 //assuming gain of 8
