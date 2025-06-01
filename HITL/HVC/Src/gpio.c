014dca3acc0a35d4379e71ab19e6d7e99ff46bc3
dddrach <102880226+dddrach@users.noreply.github.com>
2025-06-01 02:49:54 -0400

    added directory

--- HITL/HVC/Src/gpio.c
+++ HITL/HVC/Src/gpio.c
@@ -1,7 +1,7 @@
 
 
-#include <stm32f4xx_hal.h>  // HAL interface
-#include <FreeRTOS.h>   // configASSERT()
+#include "stm32f4xx_hal_conf.h"  // HAL interface
+#include "FreeRTOSConfig.h"   // configASSERT()
 
 #include "gpio.h"   // Interface to implement
 
