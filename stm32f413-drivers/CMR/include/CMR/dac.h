9bf7362d9ff9ec46b03667f87f99febc629466bb
Ayush548 <ayushgarg0728@gmail.com>
2025-09-29 15:00:23 -0400

    Added DAC Code
    Ported DAC code functionality from 24a VSM. This implementation abuses the fact that the F413 and H7 both only have DAC pins on their A port. functinality should work for both boards

---
+++ stm32f413-drivers/CMR/include/CMR/dac.h
@@ -0,0 +1,21 @@
+#pragma once
+
+/**
+ * @file dac.h
+ * @brief DAC Interface
+ *
+ * @author Carnegie Mellon Racing
+ */
+
+#ifdef HAL_DAC_MODULE_ENABLED
+
+/*
+The DACs of the stms are always on the A port. For this reason it suffices to input
+an array of the pins. For example if you wish to activate the DAC at pins PA4 and PA5
+simply input an array of [4,5]
+*/
+void dacInit(uint16_t* pins, size_t dacConfigsLen);
+void dacSetValue(uint16_t pin, uint16_t voltage_mV);
+
+#endif HAL_DAC_MODULE_ENABLED
+
