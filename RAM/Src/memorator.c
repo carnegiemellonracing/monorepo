2ec310bcdff253093aeafd70a4a206e7ace55d9a
Ayush548 <ayushgarg0728@gmail.com>
2025-12-12 03:37:00 -0500

    Clean Up Part 1

--- RAM/Src/memorator.c
+++ RAM/Src/memorator.c
@@ -29,9 +29,13 @@ void Error_Handler(void);
 // CAN can at most transmit 62500 bytes/second. Since we have 3 CAN buses this 
 // yields 1875 bytes/second that can be transmitted to us for any given 
 // 10ms interval. Therefore we give a buffer of 4096 bytes for headroom
-#define BUFFER_SIZE 512
-static uint8_t buffer[BUFFER_SIZE];
+#define BUFFER_SIZE 4096
+static uint8_t bufferA[BUFFER_SIZE];
+static uint8_t bufferB[BUFFER_SIZE];
 static uint16_t bufferLocation = 0;
+static uint8_t* txBuffer = bufferA;
+static uint8_t* rxBuffer = bufferB;
+
 
 /**
  * @brief Serialize and write a record into the memorator buffer.
@@ -55,15 +59,14 @@ void memoratorWrite(uint16_t ID, uint32_t timeStamp, uint8_t dataLength,  uint8_
     if (bufferLocation + toWrite >= BUFFER_SIZE){
         return;
     }
-
     //writes to buffer
-    memcpy(&buffer[bufferLocation], &ID, sizeof(ID));
+    memcpy(&rxBuffer[bufferLocation], &ID, sizeof(ID));
     bufferLocation += sizeof(ID);
-    memcpy(&buffer[bufferLocation], &timeStamp, sizeof(timeStamp));
+    memcpy(&rxBuffer[bufferLocation], &timeStamp, sizeof(timeStamp));
     bufferLocation += sizeof(timeStamp);
-    memcpy(&buffer[bufferLocation], &dataLength, sizeof(dataLength));
+    memcpy(&rxBuffer[bufferLocation], &dataLength, sizeof(dataLength));
     bufferLocation += sizeof(dataLength);
-    memcpy(&buffer[bufferLocation], data, dataLength);
+    memcpy(&rxBuffer[bufferLocation], data, dataLength);
     bufferLocation += dataLength;
 
 }
@@ -107,48 +110,12 @@ void memoratorInit(){
     );
 }
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
 SD_HandleTypeDef hsd;
 DMA_HandleTypeDef hdma_sdio_rx;
 DMA_HandleTypeDef hdma_sdio_tx;
 
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_DMA_Init(void);
 static void MX_SDIO_SD_Init(void);
 static void SDIO_SDCard_Test(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
@@ -156,16 +123,13 @@ static void SDIO_SDCard_Test(void);
   */
 int randomBS(void)
 {
-
-  MX_GPIO_Init();
-  MX_DMA_Init();
-MX_SDIO_SD_Init();
-  HAL_SD_MspInit(&hsd);
-  cmr_dmaInit(&hdma_sdio_rx);
+    MX_SDIO_SD_Init();
+    HAL_SD_MspInit(&hsd);
+    cmr_dmaInit(&hdma_sdio_rx);
     cmr_dmaInit(&hdma_sdio_tx);
-  MX_FATFS_Init();
-  // Test The SDIO SD Card Interface
-  SDIO_SDCard_Test();
+    MX_FATFS_Init();
+    // Test The SDIO SD Card Interface
+    SDIO_SDCard_Test();
 
   while (1)
   {
@@ -253,264 +217,113 @@ static void SDIO_SDCard_Test(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 192;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Enables the Clock Security System
-  */
-  HAL_RCC_EnableCSS();
-}
-
-/**
-  * @brief SDIO Initialization Function
+  * @brief SDIO Initialization Function. This function is call by CMR then 
+  * by the HAL separately
   * @param None
   * @retval None
   */
 static void MX_SDIO_SD_Init(void)
 {
 
-  /* USER CODE BEGIN SDIO_Init 0 */
-
-  /* USER CODE END SDIO_Init 0 */
-
-  /* USER CODE BEGIN SDIO_Init 1 */
-
-  /* USER CODE END SDIO_Init 1 */
   hsd.Instance = SDIO;
   hsd.Init.ClockEdge = SDIO_CLOCK_EDGE_RISING;
   hsd.Init.ClockBypass = SDIO_CLOCK_BYPASS_DISABLE;
   hsd.Init.ClockPowerSave = SDIO_CLOCK_POWER_SAVE_DISABLE;
   hsd.Init.BusWide = SDIO_BUS_WIDE_1B;
   hsd.Init.HardwareFlowControl = SDIO_HARDWARE_FLOW_CONTROL_DISABLE;
+  // TODO Play around with this number and see what works
   hsd.Init.ClockDiv = 118;
-  /* USER CODE BEGIN SDIO_Init 2 */
-
-  /* USER CODE END SDIO_Init 2 */
+  // TODO see if 4b is actually working??
 
 }
 
 /**
-  * Enable DMA controller clock
-  */
-static void MX_DMA_Init(void)
-{
-
-  /* DMA controller clock enable */
-  __HAL_RCC_DMA2_CLK_ENABLE();
-
-  /* DMA interrupt init */
-  /* DMA2_Stream3_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA2_Stream3_IRQn, 5, 0);
-  HAL_NVIC_EnableIRQ(DMA2_Stream3_IRQn);
-  /* DMA2_Stream6_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA2_Stream6_IRQn, 5, 0);
-  HAL_NVIC_EnableIRQ(DMA2_Stream6_IRQn);
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
-
-
-/**
-* @brief SD MSP Initialization
-* This function configures the hardware resources used in this example
+* @brief SD MSP Initialization. This function is called by the HAL when first
+* writing to an SD card and by us to configure the DMAs
+*
 * @param hsd: SD handle pointer
 * @retval None
 */
 void HAL_SD_MspInit(SD_HandleTypeDef* hsd)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
-  if(hsd->Instance==SDIO)
-  {
-  /* USER CODE BEGIN SDIO_MspInit 0 */
-
-  /* USER CODE END SDIO_MspInit 0 */
-
-  /** Initializes the peripherals clock
-  */
-    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SDIO|RCC_PERIPHCLK_CLK48;
-    PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48CLKSOURCE_PLLQ;
-    PeriphClkInitStruct.SdioClockSelection = RCC_SDIOCLKSOURCE_CLK48;
-    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if(hsd->Instance==SDIO)
     {
-      Error_Handler();
-    }
 
-    /* Peripheral clock enable */
-    __HAL_RCC_SDIO_CLK_ENABLE();
+         /* Peripheral clock enable */
+        __HAL_RCC_SDIO_CLK_ENABLE();
+        __HAL_RCC_GPIOC_CLK_ENABLE();
+        __HAL_RCC_GPIOD_CLK_ENABLE();
 
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOD_CLK_ENABLE();
+        /**SDIO GPIO Configuration
+        PD2     ------> SDIO_CMD
+        PC12     ------> SDIO_CK
+        PC8     ------> SDIO_D0
+        */
+        GPIO_InitStruct.Pin = GPIO_PIN_2;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_PULLUP;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF12_SDIO;
+        HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-    /**SDIO GPIO Configuration
-    PD2     ------> SDIO_CMD
-    PC12     ------> SDIO_CK
-    PC8     ------> SDIO_D0
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_2;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF12_SDIO;
-    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+        GPIO_InitStruct.Pin = GPIO_PIN_12;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF12_SDIO;
+        HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = GPIO_PIN_12;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF12_SDIO;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+        GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_PULLUP;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF12_SDIO;
+        HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF12_SDIO;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+        /* SDIO DMA Init */
+        /* SDIO_RX Init */
+        hdma_sdio_rx.Instance = DMA2_Stream3;
+        hdma_sdio_rx.Init.Channel = DMA_CHANNEL_4;
+        hdma_sdio_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+        hdma_sdio_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+        hdma_sdio_rx.Init.MemInc = DMA_MINC_ENABLE;
+        hdma_sdio_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+        hdma_sdio_rx.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+        hdma_sdio_rx.Init.Mode = DMA_PFCTRL;
+        hdma_sdio_rx.Init.Priority = DMA_PRIORITY_LOW;
+        hdma_sdio_rx.Init.FIFOMode = DMA_FIFOMODE_ENABLE;
+        hdma_sdio_rx.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_FULL;
+        hdma_sdio_rx.Init.MemBurst = DMA_MBURST_INC4;
+        hdma_sdio_rx.Init.PeriphBurst = DMA_PBURST_INC4;
+        if (HAL_DMA_Init(&hdma_sdio_rx) != HAL_OK)
+            cmr_panic("SD Card Tx DMA setup failed!");
 
-    /* SDIO DMA Init */
-    /* SDIO_RX Init */
-    hdma_sdio_rx.Instance = DMA2_Stream3;
-    hdma_sdio_rx.Init.Channel = DMA_CHANNEL_4;
-    hdma_sdio_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
-    hdma_sdio_rx.Init.PeriphInc = DMA_PINC_DISABLE;
-    hdma_sdio_rx.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_sdio_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
-    hdma_sdio_rx.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
-    hdma_sdio_rx.Init.Mode = DMA_PFCTRL;
-    hdma_sdio_rx.Init.Priority = DMA_PRIORITY_LOW;
-    hdma_sdio_rx.Init.FIFOMode = DMA_FIFOMODE_ENABLE;
-    hdma_sdio_rx.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_FULL;
-    hdma_sdio_rx.Init.MemBurst = DMA_MBURST_INC4;
-    hdma_sdio_rx.Init.PeriphBurst = DMA_PBURST_INC4;
-    if (HAL_DMA_Init(&hdma_sdio_rx) != HAL_OK)
-    {
-      Error_Handler();
-    }
+        __HAL_LINKDMA(hsd,hdmarx,hdma_sdio_rx);
 
-    __HAL_LINKDMA(hsd,hdmarx,hdma_sdio_rx);
+        /* SDIO_TX Init */
+        hdma_sdio_tx.Instance = DMA2_Stream6;
+        hdma_sdio_tx.Init.Channel = DMA_CHANNEL_4;
+        hdma_sdio_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+        hdma_sdio_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+        hdma_sdio_tx.Init.MemInc = DMA_MINC_ENABLE;
+        hdma_sdio_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+        hdma_sdio_tx.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+        hdma_sdio_tx.Init.Mode = DMA_PFCTRL;
+        hdma_sdio_tx.Init.Priority = DMA_PRIORITY_MEDIUM;
+        hdma_sdio_tx.Init.FIFOMode = DMA_FIFOMODE_ENABLE;
+        hdma_sdio_tx.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_FULL;
+        hdma_sdio_tx.Init.MemBurst = DMA_MBURST_INC4;
+        hdma_sdio_tx.Init.PeriphBurst = DMA_PBURST_INC4;
+        if (HAL_DMA_Init(&hdma_sdio_tx) != HAL_OK)
+            cmr_panic("SD Card Tx DMA setup failed!");
 
-    /* SDIO_TX Init */
-    hdma_sdio_tx.Instance = DMA2_Stream6;
-    hdma_sdio_tx.Init.Channel = DMA_CHANNEL_4;
-    hdma_sdio_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
-    hdma_sdio_tx.Init.PeriphInc = DMA_PINC_DISABLE;
-    hdma_sdio_tx.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_sdio_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
-    hdma_sdio_tx.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
-    hdma_sdio_tx.Init.Mode = DMA_PFCTRL;
-    hdma_sdio_tx.Init.Priority = DMA_PRIORITY_MEDIUM;
-    hdma_sdio_tx.Init.FIFOMode = DMA_FIFOMODE_ENABLE;
-    hdma_sdio_tx.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_FULL;
-    hdma_sdio_tx.Init.MemBurst = DMA_MBURST_INC4;
-    hdma_sdio_tx.Init.PeriphBurst = DMA_PBURST_INC4;
-    if (HAL_DMA_Init(&hdma_sdio_tx) != HAL_OK)
-    {
-      Error_Handler();
-    }
+        __HAL_LINKDMA(hsd,hdmatx,hdma_sdio_tx);
 
-    __HAL_LINKDMA(hsd,hdmatx,hdma_sdio_tx);
-
-    /* SDIO interrupt Init */
-    HAL_NVIC_SetPriority(SDIO_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(SDIO_IRQn);
-  /* USER CODE BEGIN SDIO_MspInit 1 */
-
-  /* USER CODE END SDIO_MspInit 1 */
+        /* SDIO interrupt Init */
+        HAL_NVIC_SetPriority(SDIO_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(SDIO_IRQn);
   }
-
 }
 
 
@@ -519,11 +332,5 @@ void HAL_SD_MspInit(SD_HandleTypeDef* hsd)
   */
 void SDIO_IRQHandler(void)
 {
-  /* USER CODE BEGIN SDIO_IRQn 0 */
-
-  /* USER CODE END SDIO_IRQn 0 */
-  HAL_SD_IRQHandler(&hsd);
-  /* USER CODE BEGIN SDIO_IRQn 1 */
-
-  /* USER CODE END SDIO_IRQn 1 */
+    HAL_SD_IRQHandler(&hsd);
 }
