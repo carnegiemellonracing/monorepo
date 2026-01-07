e732f78eb18616abec4db87951b14b4cdd78ab7a
lijasq <li.jasmine.q@gmail.com>
2026-01-07 00:26:26 -0500

    added can ids

---
+++ DCM/Src/sensoric.c
@@ -0,0 +1,18 @@
+/**
+ * @file sensoric.c
+ * @brief Board-specific senesoric implementation.
+ *
+ *
+ * @author Carnegie Mellon Racing
+ */
+
+#include "sensoric.h"
+#include <CMR/can_types.h> 
+#include <math.h>
+
+void sensoric_parse(uint16_t canID, volatile void *payload) {
+
+    canVehicleRx_t sensoric_msg;
+
+    
+}
