4036e53f34a21a456d61e3470263116919f59d84
lijasq <li.jasmine.q@gmail.com>
2026-01-08 22:35:03 -0500

    removed movella lines from .h

--- DCM/Inc/sensoric.h
+++ DCM/Inc/sensoric.h
@@ -1,124 +1,82 @@
-#ifndef MOVELLA_H
-#define MOVELLA_H
+#ifndef SENSORIC_H
+#define SENSORIC_H
 
 #include "can.h"
-
-typedef enum {
-    MOVELLA_QUATERNION = 0,
-    MOVELLA_EULER_ANGLES,
-    MOVELLA_IMU_ACCEL,
-    MOVELLA_IMU_GYRO,
-    MOVELLA_VELOCITY,
-    MOVELLA_MESSAGE_NUM,
-} movella_message_t;
-
-static const float scaling_constants[MOVELLA_MESSAGE_NUM] = {
-    [MOVELLA_QUATERNION] = 1 / (float) (1U << 16 - 1),
-    [MOVELLA_EULER_ANGLES] = 1 / (float) (1U << 7),
-    [MOVELLA_IMU_ACCEL] = 1 / (float) (1U << 8),
-    [MOVELLA_IMU_GYRO] = 1 / (float) (1U << 9),
-    [MOVELLA_VELOCITY] = 1 / (float) (1U << 6),
-};
-
-typedef float matrix3x3[3][3];
-typedef float vector3[3];
-
-static const matrix3x3 R_BD = {
-    {1.0f, 0.0f, 0.0f},
-    {0.0f, 1.0f, 0.0f},
-    {0.0f, 0.0f, 1.0f},
-};
-
-static const vector3 x_BD = { -0.5f, 0.0f, 0.2f };
-
-// Car COG frame
-static const vector3 x_FL = { 0.837f, 0.65f, -0.3f };
-static const vector3 x_FR = { 0.837f, -0.65f, -0.3f };
-static const vector3 x_RL = { -0.713f, 0.65f, -0.3f };
-static const vector3 x_RR = { -0.713f, -0.65f, -0.3f };
-
-static const vector3 x_D_FL = { x_FL[0] - x_BD[0], x_FL[1] - x_BD[1], x_FL[2] - x_BD[2] };
-static const vector3 x_D_FR = { x_FR[0] - x_BD[0], x_FR[1] - x_BD[1], x_FR[2] - x_BD[2] };
-static const vector3 x_D_RL = { x_RL[0] - x_BD[0], x_RL[1] - x_BD[1], x_RL[2] - x_BD[2] };
-static const vector3 x_D_RR = { x_RR[0] - x_BD[0], x_RR[1] - x_BD[1], x_RR[2] - x_BD[2] };
-
 
 typedef struct {
 
     struct {
-        float x;
-        float y;
-        float a;
-        float ang_s;
+        int16_t x;
+        int16_t y;
+        int16_t a;
+        int16_t ang_s;
     } vel_ang_poi;
 
     struct {
-        float dist_a;
-        float radius;
-        float acc_c
+        int32_t dist_a;
+        int16_t radius;
+        int16_t acc_c
     } dist_poi;
     
     struct {
-        float roll;
-        float pitch;
+        int16_t roll;
+        int16_t pitch;
     } pitch_roll;
     
     struct {
-        float x;
-        float y;
-        float z;
+        int16_t x;
+        int16_t y;
+        int16_t z;
     } acc_hor;
 
     struct {
-        float x;
-        float y;
-        float z;
+        int16_t x;
+        int16_t y;
+        int16_t z;
     } rate_hor;
     
     struct {
-        float x;
-        float y;
-        float z;
-        float ang_s;
+        int16_t x;
+        int16_t y;
+        int16_t z;
+        int16_t ang_s;
     } vel_ang;
 
     struct {
-        float dist_a;
-        float radius;
-        float acc_c;
+        int32_t dist_a;
+        int16_t radius;
+        int16_t acc_c;
     } dist;
 
     struct {
-        float x;
-        float y;
-        float z;
+        int16_t x;
+        int16_t y;
+        int16_t z;
     } acc;
 
     struct {
-        float x;
-        float y;
-        float z;
+        int16_t x;
+        int16_t y;
+        int16_t z;
     } rate;
 
     struct {
-        float vel_a;
-        float vel_s;
-        float quality_ch0;
-        float quality_ch1;
+        int16_t vel_a;
+        int16_t vel_s;
+        uint16_t quality_ch0;
+        uint16_t quality_ch1;
     } vel_ang_sp;
 
     struct {
-        float dist_a;
-        float vel_x;
-        float vel_y;
+        int32_t dist_a;
+        int16_t vel_x;
+        int16_t vel_y;
     } dist_vel_sp;
 
 } sensoric_state_t;
 
-extern volatile movella_state_t movella_state;
+extern volatile sensoric_state_t sensoric_state;
 
-void movella_parse(uint16_t canID, volatile void *payload);
-void movella_test();
-void movella_random_test();
+void sensoric_parse(uint16_t canID, volatile void *payload);
 
 #endif
