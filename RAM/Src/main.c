71388120406c78ff59d4c589a91895fdacc59071
Ayush548 <ayushgarg0728@gmail.com>
2026-03-06 15:09:44 -0500

    Added RTC to drivers and cleaned up code, NOT TESTED ON BOARDS

--- RAM/Src/main.c
+++ RAM/Src/main.c
@@ -5,25 +5,27 @@
  * @author Carnegie Mellon Racing
  */
 
-#include <stm32f4xx_hal.h>  // HAL interface
-
-#include <CMR/panic.h>  // cmr_panic()
-#include <CMR/rcc.h>    // RCC interface
+// CMR framework
 #include <CMR/can.h>    // CAN interface
 #include <CMR/gpio.h>   // GPIO interface
+#include <CMR/panic.h>  // cmr_panic()
+#include <CMR/rcc.h>    // RCC interface
+#include <CMR/rtc.h>    // RTC interface
+
 #include <CMR/tasks.h>  // Task interface
 
+// Middleware
+#include "fatfs.h"      // middleware for file system provided by ST
 
-#include "can.h"    // Board-specific CAN interface
-#include "config.h" // Previous flash configuration
-#include "fatfs.h"   // middleware for file system provided by ST
-#include "gpio.h"   // Board-specific GPIO interface
-#include "memorator.h"   // Board-specific GPIO interface
-#include "parser.h" // JSON configuration
-#include "sample.h" // CBOR encoding
-#include "statusLED.h"   // Board-specific statusLED interface
-#include "uart.h"   // Board-specific UART interface
-#include "rtc.h"
+// Project headers
+#include "can.h"        // Board-specific CAN interface
+#include "config.h"     // Previous flash configuration
+#include "gpio.h"       // Board-specific GPIO interface
+#include "memorator.h"  // Board-specific GPIO interface
+#include "parser.h"     // JSON configuration
+#include "sample.h"     // CBOR encoding
+#include "statusLED.h"  // Board-specific statusLED interface
+#include "uart.h"       // Board-specific UART interface
 
 
 /**
@@ -37,7 +39,8 @@ int main(void) {
     // System initialization.
     HAL_Init();
     cmr_rccSystemClockEnable();
-    // SystemClock_Config();
+    cmr_rtc_init();
+
 
     // Peripheral configuration.
     uartInit();
@@ -46,7 +49,6 @@ int main(void) {
     statusLEDInit();
     memoratorInit();
 
-    // MX_RTC_Init();
     // Load in JSON configuration
     parserInit();
     // Set up CBOR encoder
--- stm32f413-drivers/CMR/src/f413.c
+++ stm32f413-drivers/CMR/src/f413.c
@@ -273,7 +273,7 @@ void _platform_rccSystemClockEnable(void)
     __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
     // Initializes the CPU, AHB and APB busses clocks
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE;
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_LSE;
     RCC_OscInitStruct.HSEState = RCC_HSE_ON;
     RCC_OscInitStruct.LSEState = RCC_LSE_ON;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -288,14 +288,14 @@ void _platform_rccSystemClockEnable(void)
         cmr_panic("HAL_RCC_OscConfig() failed!");
     }
 
-    // Initializes the CPU, AHB and APB busses clocks
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
     {
         cmr_panic("HAL_RCC_ClockConfig() failed!");
