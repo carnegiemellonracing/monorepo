490e06a1836e840868090f50a9f7f33119ceb00f
Ayush548 <ayushgarg0728@gmail.com>
2026-03-06 23:51:27 -0500

    Lightened Precharge

--- HVC/Src/state_task.c
+++ HVC/Src/state_task.c
@@ -10,7 +10,7 @@
 
 static cmr_canHVCState_t currentState = CMR_CAN_HVC_STATE_ERROR;
 
-#define PRECHARGE_THRESH 57000
+#define PRECHARGE_THRESH 40000
 #define MIN_PACK_THRESH 360000
 
 static bool cellBalancing = false; 
@@ -35,7 +35,7 @@ static bool prechargeDone() {
     int32_t HVmillivolts = getHVmillivolts();
     volatile cmr_canHVBMSPackVoltage_t *HVBMSPackVoltage = getPayload(CANRX_HVBMS_PACKVOLT); 
     TickType_t lastWakeTime = xTaskGetTickCount();
-    return (abs(HVBMSPackVoltage->battVoltage_mV - HVmillivolts) < 30000 &&
+    return (abs(HVBMSPackVoltage->battVoltage_mV - HVmillivolts) < PRECHARGE_THRESH &&
             HVBMSPackVoltage->battVoltage_mV > MIN_PACK_THRESH &&
             !cmr_canRXMetaTimeoutError(&canRXMeta[CANRX_HEARTBEAT_HVBMS], lastWakeTime) &&
             !cmr_canRXMetaTimeoutError(&canRXMeta[CANRX_HVBMS_PACKVOLT], lastWakeTime));
