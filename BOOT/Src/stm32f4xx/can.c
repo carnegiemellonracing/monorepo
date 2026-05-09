a14527671ab32170a5115a7627d1a7cfd8790710
Michael Honaker <mchonaker@gmail.com>
2026-05-09 13:45:53 -0400

    Update can config

--- BOOT/Src/stm32f4xx/can.c
+++ BOOT/Src/stm32f4xx/can.c
@@ -28,13 +28,13 @@ void canInit(void) {
         .Mode = GPIO_MODE_AF_PP,
         .Pull = GPIO_NOPULL,
         .Speed = GPIO_SPEED_FREQ_VERY_HIGH,
-        .Alternate = cmr_canGPIOAF(instance, rxPort)
+        .Alternate = GPIO_AF9_CAN1,
     };
     HAL_GPIO_Init(rxPort, &pinConfig);
 
     // Configure CAN TX pin.
     pinConfig.Pin = txPin;
-    pinConfig.Alternate = cmr_canGPIOAF(instance, txPort);
+    pinConfig.Alternate = GPIO_AF9_CAN1;
     HAL_GPIO_Init(txPort, &pinConfig);
 }
 
