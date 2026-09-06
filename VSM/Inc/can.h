d9420b8a098103fb23281902f6b118c14bab3b61
sophiaxd0 <sophia.deng.05@gmail.com>
2026-09-05 20:01:55 -0400

    revert vsm

--- VSM/Inc/can.h
+++ VSM/Inc/can.h
@@ -44,7 +44,6 @@ typedef enum {
     CANRX_RR_ERPM,
     CANRX_AS_MISSION_FINISHED,
     CANRX_AMS_ERROR,
-    CANRX_MEMORATOR_WARNINGS, 
     CANRX_LEN     /**< @brief Number of periodic CAN messages. */
 } canRX_t;
 
