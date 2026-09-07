12ff93c0234b11d943f206e625afcbb55900403d
sxdong-gif <sxdong@andrew.cmu.edu>
2026-09-07 12:35:54 -0400

    sensors .h and .c consolidation

--- VCU/Inc/sensors.h
+++ VCU/Inc/sensors.h
@@ -7,16 +7,31 @@
 
 #pragma once
 
+#ifndef SENSORS_H
+#define SENSORS_H
+
 #include <stdbool.h>
 
 #include <CMR/sensors.h>
 #include "adc.h"
 
+//DCM Values
 /** @brief Array indexes for sensor value calibration array. */
 typedef enum {
 	SENSOR_CH_VOLTAGE_CV = 0,   /**< @brief High voltage rail. */
 	SENSOR_CH_AVG_CURRENT_DA,   /**< @brief HV current draw. */
     SENSOR_CH_LEN               /**< @brief Number of sensors. */
+} sensorChannel_t;
+
+//VSM Values
+typedef enum {
+    SENSOR_CH_HALL_EFFECT_A = 0,    /**< @brief Hall effect sensor for accumulator current. */
+    SENSOR_CH_BPRES_PSI,            /**< @brief Rear brake pressure sensor. */
+    SENSOR_CH_VOLTAGE_MV,           /**< @brief Board voltage sense. */
+    SENSOR_CH_CURRENT_MA,           /**< @brief Board current sense. */
+    SENSOR_CH_SS_IN,                /**< @brief Safety Circuit voltage before latches. */
+    SENSOR_CH_SS_OUT,               /**< @brief SS voltage after latches. */
+    SENSOR_CH_LEN                   /**< @brief Total number of sensors. */
 } sensorChannel_t;
 
 extern cmr_sensorList_t sensorList;
@@ -29,4 +44,4 @@ int32_t getVoltage_mV(void);
 
 int32_t getCurrent_mA(void);
 
-
+#endif /* SENSORS_H */
--- VCU/Src/sensors.c
+++ VCU/Src/sensors.c
@@ -21,9 +21,20 @@ bool use_emd = false;
 /**
  * @brief Mapping of sensor channels to ADC channels.
  */
+ //DCM Values
 const adcChannel_t sensorsADCChannels[SENSOR_CH_LEN] = {
     [SENSOR_CH_VOLTAGE_CV]  = ADC_THERM1,
     [SENSOR_CH_AVG_CURRENT_DA]  = ADC_THERM2
+};
+
+//VSM Values
+static const adcChannel_t sensorsADCChannels[SENSOR_CH_LEN] =
+{
+    [SENSOR_CH_HALL_EFFECT_A] = ADC_HALL_EFFECT,
+    [SENSOR_CH_BPRES_PSI]      = ADC_REAR_BRAKE_PRES,
+    [SENSOR_CH_VOLTAGE_MV]     = ADC_VSENSE,
+    [SENSOR_CH_SS_IN]          = ADC_SSIN,
+    [SENSOR_CH_SS_OUT]         = ADC_SSOUT
 };
 
 /** @brief forward declaration */
@@ -78,6 +89,60 @@ static int32_t adcToAvgBusCurrent_cA(const cmr_sensor_t *sensor, uint32_t readin
     return (int32_t) (reading); //* 100.0f);
 }
 
+//VSM Sensors
+// TODO calibrate all of these min/max values
+cmr_sensor_t sensors[SENSOR_CH_LEN] = {
+    [SENSOR_CH_HALL_EFFECT_A] = {
+        .sample = sampleADCSensor,
+        .conv = adc_to_hv_current,
+        .readingMin = 0,            // TODO
+        .readingMax = CMR_ADC_MAX,  // TODO
+        .outOfRange_pcnt = 10,
+        .errorFlag = CMR_CAN_ERROR_NONE
+    },
+    [SENSOR_CH_BPRES_PSI] = {
+        .sample = sampleADCSensor,
+        .conv = adcToBrakePres_PSI,
+        .readingMin = 0,            // TODO
+        .readingMax = CMR_ADC_MAX,  // TODO
+        .outOfRange_pcnt = 10,
+        .errorFlag = CMR_CAN_ERROR_VSM_BPRES
+    },
+    [SENSOR_CH_VOLTAGE_MV] = {
+        .conv = adcToBusVoltage_mV,
+        .sample = sampleADCSensor,
+        .readingMin = 2256, // 20 Volts
+        .readingMax = 2933, // 26 Volts
+        .outOfRange_pcnt = 10,
+        .warnFlag = CMR_CAN_WARN_BUS_VOLTAGE
+    },
+    [SENSOR_CH_CURRENT_MA] = {
+        .conv = adcToBusCurrent_mA,
+        .sample = sampleADCSensor,
+        .readingMin = 250,  // 10 mA
+        .readingMax = 2500, // 100 mA
+        .outOfRange_pcnt = 10,
+        .warnFlag = CMR_CAN_WARN_BUS_CURRENT
+    },
+    [SENSOR_CH_SS_IN] = {
+        .conv = adcToBusVoltage_eight_V,
+        .sample = sampleADCSensor,
+        .readingMin = 250,  
+        .readingMax = 2933, // 26 Volts
+        .outOfRange_pcnt = 10,
+        .warnFlag = CMR_CAN_WARN_BUS_VOLTAGE
+    },
+    [SENSOR_CH_SS_OUT] = {
+        .conv = adcToBusVoltage_eight_V,
+        .sample = sampleADCSensor,
+        .readingMin = 250,  // 10 mA
+        .readingMax = 2933, // 26 Volts
+        .outOfRange_pcnt = 10,
+        .warnFlag = CMR_CAN_WARN_BUS_CURRENT
+    },
+};
+
+//DCM Sensors
 static cmr_sensor_t sensors[SENSOR_CH_LEN] = {
     [SENSOR_CH_VOLTAGE_CV] = {
         .conv = adcToBusVoltage_cV,
@@ -97,6 +162,102 @@ static cmr_sensor_t sensors[SENSOR_CH_LEN] = {
     }
 };
 
+/**
+ * @brief Converts a raw sensor value into a low-voltage bus voltage.
+ *
+ * @param sensor The sensor.
+ * @param value The raw value.
+ *
+ * @return Voltage in mV.
+ */
+static int32_t adcToBusVoltage_mV(const cmr_sensor_t *sensor, uint32_t value) {
+    (void) sensor;  // Placate compiler.
+
+    // value * 0.8 (mV per bit) * 11 (1:11 voltage divider)
+    uint32_t busVoltage_mV = value * 8 * 11 / 10;
+    return (int32_t) busVoltage_mV;
+}
+
+/**
+ * @brief Converts a raw sensor value into a low-voltage bus voltage.
+ *
+ * @param sensor The sensor.
+ * @param value The raw value.
+ *
+ * @return Voltage in qV.
+ */
+static int32_t adcToBusVoltage_eight_V(const cmr_sensor_t *sensor, uint32_t value) {
+    (void) sensor;  // Placate compiler.
+    uint32_t busVoltage_mV = adcToBusVoltage_mV(sensor, value);
+    return busVoltage_mV / 125;
+}
+
+/**
+ * @brief Converts a raw sensor value to a brake pressure in PSI.
+ *
+ * @param sensor The sensor.
+ * @param value The raw value.
+ *
+ * @return Front brake pressure in PSI.
+ */
+static int32_t adcToBrakePres_PSI(const cmr_sensor_t *sensor, uint32_t value) {
+    // https://www.variohm.com/images/datasheets/EPT3100_0113_F_1.pdf
+    // EPT3100-H-10000 is (0, 100) bar, (0.5, 4.5) V
+    // Divider is 5-to-3.3 V -> (0.333, 3) V
+    // 100 bar is 1450 PSI
+    static const uint32_t offset = 360;     // 0.333 V offset
+
+    (void) sensor;  // Placate compiler.
+
+    if (value < offset) {
+        // Clamp to 0.
+        value = offset;
+    }
+
+    uint32_t brakePres_PSI = (value - offset) * 1450 / 3313;
+    return (int32_t) brakePres_PSI;
+}
+
+/**
+ * @brief Converts a raw sensor value into a low-voltage bus current.
+ *
+ * @param sensor The sensor.
+ * @param value The raw value.
+ *
+ * @return Current in mA.
+ */
+static int32_t adcToBusCurrent_mA(const cmr_sensor_t *sensor, uint32_t value) {
+    (void) sensor;  // Placate compiler.
+
+    /* value * 0.8 (mV per bit) / 20 (gain of current shunt monitor)
+     * http://www.ti.com/lit/ds/symlink/ina196.pdf
+     * page 3 section 5 for INA196 */
+    uint32_t busCurrent_mA = value * 8 / 10 / 20;
+    return (int32_t) busCurrent_mA;
+}
+
+/**
+ * @brief Converts a raw sensor value into accumulator current draw.
+ *
+ * @param sensor The sensor.
+ * @param value The raw value.
+ *
+ * @return Current in amps
+ */
+static int32_t adc_to_hv_current(const cmr_sensor_t *sensor, uint32_t value) {
+    (void) sensor;  // Placate compiler.
+
+    // https://www.lem.com/sites/default/files/products_datasheets/ho_50_250-s-0100_series.pdf
+    float mcu_volts_per_adc = 3.3f / 4096.0f; 
+    float offset_mcu_volts = 1.568f; // Reading at 0 amps off car
+    float amps_per_sensor_volt = 125.0f / 2.0f;
+    float sensor_volts_per_mcu_volt = 5.51f / 3.3f; // Based on voltage divider
+
+    float mcu_volts_no_offset = value * mcu_volts_per_adc;
+    float mcu_volts = mcu_volts_no_offset - offset_mcu_volts;
+    float sensor_volts = mcu_volts * sensor_volts_per_mcu_volt;
+    return (int8_t) (amps_per_sensor_volt * sensor_volts);
+}
 
 /**
  * @brief Gets the voltage from your preferred source
